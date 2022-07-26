b0b4bace29de76a4058eb1c0c09d3d320b11815f
Fernando Nascimento <fernando_dev@outlook.com.br>
2022-07-26 19:16:22 -0300

    criacao navigation, service, di

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,13 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+    const val PUBLIC_KEY = "515952c40a29b3c5a34c2970e0242345"
+    const val PRIVATE_KEY = "181e1972dca3d7a3e2d385a13e1427b5c0e78916"
+    const val API_KEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+
+
+}
